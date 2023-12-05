3d67cfa4d63b2fb1268d10823447de186d709ae2
SarahJWeisberg <sarah.j.weisberg@stonybrook.edu>
2023-12-05 13:24:18 -0500

    fixed errors in check.rpath.params

--- Sarah_R/rebalance.R
+++ Sarah_R/rebalance.R
@@ -1,5 +1,23 @@
-#test
+#Code to rebalance 1980-85 Mid-Atlantic Bight Rpath model
+#Goal is to align starting model with Gulf of Maine, Georges Bank models
+
+#Author: Sarah J. Weisberg
+
+# Tue Dec  5 12:54:24 2023 ------------------------------
 
 #load packages
+library(here)
+library(Rpath)
+library(dplyr)
 
 #load Brandon's model
+load(here("MAB Rpath - BALANCED - July 2022.RData"))
+
+#correct detritus, discards BA, should be 0
+#had been NA
+MAB.rpath.params[["model"]][["BioAcc"]][50:51] <- 0
+
+#correct fishing matrix
+#fleets are catching other fleets
+#issue is with rec fishery
+MAB.rpath.params[["model"]][["Recreational"]][52:62] <-NA
