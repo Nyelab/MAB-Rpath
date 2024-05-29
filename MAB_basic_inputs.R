bfaf067207767e0b90facadb22dcf3fa81e2eec1
Max Grezlik <maxgrezlik@gmail.com>
2024-05-29 11:58:44 -0400

    Added 'OceanQuahog' and 'SurfClam' to MAB.groups

--- MAB_basic_inputs.R
+++ MAB_basic_inputs.R
@@ -20,7 +20,18 @@ library(data.table);library(here)
 
 ## ---------------------------
 
-MAB.groups<-as.data.table(c('AmLobster','RiverHerring','AtlCroaker','AtlMackerel','AtlScallop','Bacteria','BaleenWhales','BlackSeaBass','Bluefish','Butterfish','Cod','Fourspot','GelZooplankton','Goosefish','HMS','Illex','LgCopepods','LittleSkate','Loligo','Macrobenthos','Megabenthos','Mesopelagics','Micronekton','Microzooplankton','OceanPout','Odontocetes','OtherCephalopods','OtherDemersals','OtherPelagics','OtherShrimps','OtherSkates','Phytoplankton','RedHake','Scup','SeaBirds','Sharks','SilverHake','SmCopepods','SmFlatfishes','SmoothDogfish','SmPelagics','SouthernDemersals','SpinyDogfish','SummerFlounder','Weakfish','Windowpane','WinterFlounder','WinterSkate','YTFlounder','Krill','AtlMenhaden'))
+MAB.groups<-as.data.table(c('AmLobster','RiverHerring','AtlCroaker','AtlMackerel',
+                            'AtlScallop','Bacteria','BaleenWhales','BlackSeaBass',
+                            'Bluefish','Butterfish','Cod','Fourspot','GelZooplankton',
+                            'Goosefish','HMS','Illex','LgCopepods','LittleSkate','Loligo',
+                            'Macrobenthos','Megabenthos','Mesopelagics','Micronekton',
+                            'Microzooplankton','OceanPout','OceanQuahog','Odontocetes',
+                            'OtherCephalopods','OtherDemersals','OtherPelagics','OtherShrimps',
+                            'OtherSkates','Phytoplankton','RedHake','Scup','SeaBirds','Sharks',
+                            'SilverHake','SmCopepods','SmFlatfishes','SmoothDogfish',
+                            'SmPelagics','SouthernDemersals','SpinyDogfish',
+                            'SummerFlounder','SurfClam', 'Weakfish','Windowpane',
+                            'WinterFlounder','WinterSkate','YTFlounder','Krill','AtlMenhaden'))
 setnames(MAB.groups,'V1','RPATH')
 MAB.strata<-c(1010:1080, 1100:1120, 1600:1750, 3010:3450, 3470, 3500, 3510)
 MAB.stareas<-c(537, 539, 600, 612:616, 621, 622, 625, 626, 631, 632)
